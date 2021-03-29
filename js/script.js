c0ec1251b3f2e1026f18005f9b4c1decd79bbcfb
Matia <matia.casu@gmail.com>
2021-03-29 18:36:28 +0200

    Stampata rubrica tramite ciclo v-for

---
+++ js/script.js
@@ -0,0 +1,27 @@
+var app = new Vue({
+  el: "#app",
+  data: {
+    contacts: [
+    {
+      nome: "Isabel",
+      srcImg: "img/avatar_1.jpg",
+    },
+    {
+      nome: "Nicola",
+      srcImg: "img/avatar_3.jpg",
+    },
+    {
+      nome: "Arianna",
+      srcImg: "img/avatar_2.jpg",
+    },
+    {
+      nome: "Francesca",
+      srcImg: "img/avatar_7.jpg",
+    },
+    {
+      nome: "Mario",
+      srcImg: "img/avatar_6.jpg",
+    }
+    ]
+  }
+});

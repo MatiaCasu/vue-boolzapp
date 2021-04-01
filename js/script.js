a7ee10eccceeffecebbe65896a66a5ffe8f8cec2
Matia <matia.casu@gmail.com>
2021-04-01 17:41:28 +0200

    Sistemato layout messaggi in main-chat

--- js/script.js
+++ js/script.js
@@ -40,7 +40,11 @@ var app = new Vue({
         },
         {
           text: "provaaaaaah"
-        }]
+        },
+        {
+          text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        }
+      ]
       },
   ]},
   methods: {
@@ -48,8 +52,8 @@ var app = new Vue({
       array.forEach((element)=>{
         element.activeClass = "";
       });
-      array[fIndex].activeClass = "active";
       this.index = fIndex;
+      array[fIndex].activeClass = "active";
     }
   }
 });
